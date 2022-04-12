02802fa355fd4a77c554a2987d416ef9edee7a14
beginnerpgmr <komnakyung@naver.com>
2022-04-13 01:34:57 +0900

    pet2

--- bgnnr.py
+++ bgnnr.py
@@ -1,6 +1,14 @@
 #애완동물
 
 animal = "고슴도치"
+name = "까미"
+age = 5
+hobby = "쳇바퀴"
+is_adult = age >= 4
 
 print("나는 "+animal+"를 키운다")
-print(animal+"은 날카로운 가시를 가지고 있다")
+print(animal+"의 이름은 "+name+"이다")
+print(name+"는 날카로운 가시를 가지고 있다")
+print(name+"는 "+str(age)+"살이며, "+hobby+"를 아주 좋아한다")
+print(name+"는 어른일까?" +str(age))
+
