13708b79b5bb888719ad36e92ff3772500171fc4
beginnerpgmr <komnakyung@naver.com>
2022-04-14 20:26:33 +0900

    3-3숫자처리함수

---
+++ 3-3숫자처리함수.py
@@ -0,0 +1,11 @@
+print(abs(-5)) # abs : 절댓값, -5의 절댓값은 5
+print(pow(4, 2)) # pow : 제곱값, 4의 2승(4^2)
+print(max(5, 12)) # max : 최대값 알려줌, 5와 12중 최대값은 12
+print(min(5, 12)) # min : 최소값 알려줌, 5와 12중 최소값은 5
+print(round(3.14)) # round : 반올림, 3.14의 반올림값은 3
+print(round(4.6))
+
+from math import* # math 라이브러리에 있는 모든 것을 쓰겠다
+print(floor(4.99)) # floor : 내림. 4.99를 내림하면 4
+print(ceil(3.14)) #ceil : 올림. 3.14를 올림하면 4
+print(sqrt(16)) # sqrt : 제곱근 구함. 16은 4의 제곱근
