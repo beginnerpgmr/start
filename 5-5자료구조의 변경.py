3c37a31ed24616be008a2f5d035be9a822a10463
beginnerpgmr <komnakyung@naver.com>
2022-04-26 23:36:32 +0900

    5장

---
+++ 5-5자료구조의 변경.py
@@ -0,0 +1,18 @@
+# 자료구조의 변경
+# 커피숍
+menu = {"커피", "우유", "주스"}
+print(menu, type(menu)) # 중괄호{}로 출력
+
+menu = list(menu)
+print(menu, type(menu)) # 대괄호[]로 출력
+
+menu = tuple(menu)
+print(menu, type(menu)) # 소괄호()로 출력
+
+menu = set(menu)
+print(menu, type(menu))
+
+
+
+
+
